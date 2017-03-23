3595eee941c087647182507887a9e571b6e93f4d
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-03-23 23:57:23 +1300

    model for gameboard, deprecating Blocks

---
+++ Warlords/src/BoardModel.java
@@ -0,0 +1,17 @@
+/* Methods and variables of the gameplay board (768x768 square) 
+ * not sure if the whole 1024x768 frame should be here? for HUD/GUI purposes 
+ * idk if we need this at this point i feel like it's easier if we put all the paddle/ball/bricks in one model class??? */ 
+public class BoardModel {
+	int width = 768; // 1024 if including HUD? 
+	int height = 768;
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	
+}
