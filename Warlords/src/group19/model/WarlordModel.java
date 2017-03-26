f64c07ba3c1ae8c2ebd8ddd52aad6fb46a97458c
Serena Toon <stoo718@aucklanduni.ac.nz>
2017-03-27 01:14:56 +1300

    added 'player' data/methods to WarlordModel

--- Warlords/src/group19/model/WarlordModel.java
+++ Warlords/src/group19/model/WarlordModel.java
@@ -5,6 +5,8 @@ public class WarlordModel {
 	private int yPos;
 	private boolean isDead; // might rename, currently same name as method 
 	private boolean isWinner;
+	private int score;
+	private boolean isAI;
 	
 	//constructor 
 	public WarlordModel(int x, int y) {
@@ -12,6 +14,10 @@ public class WarlordModel {
 		yPos = y;
 		isDead = false;
 		isWinner = false;
+	}
+	
+	public boolean isAI() { // returns whether or not player is AI (false if human-controllable player) 
+		return isAI;
 	}
 	
 	// x and y setters probably not necessary? since x and y position should only be set once at beginning -- done so in constructor 
@@ -30,6 +36,18 @@ public class WarlordModel {
 	
 	public int getYPos() {
 		return yPos;
+	}
+	
+	public void setScore(int score_) {
+		score = score_;
+	}
+	
+	public void addScore() {
+		score += 10; // how many points should you get for destroying 1 brick? 
+	}
+	
+	public int getScore() {
+		return score;
 	}
 	
 	public boolean isDead() {
