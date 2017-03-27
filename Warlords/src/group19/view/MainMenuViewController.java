3d86c3b839ddaffabd22604c3a0647501a11d8e2
Marcus Wong <marcuswong@hotmail.com>
2017-03-27 22:56:10 +1300

    added some dank sounds on click, like previous implementation. now I shall finally go and study for 313

--- Warlords/src/group19/view/MainMenuViewController.java
+++ Warlords/src/group19/view/MainMenuViewController.java
@@ -6,10 +6,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
 
 public class MainMenuViewController implements Initializable, ViewInterface { //ViewInterface passes stuff from ViewLoader into these controllers
 	ViewLoader loader;
-	
+    AudioClip modeSelect = new AudioClip(GameMenuView.class.getClassLoader().getResource("res/sounds/game_start.mp3").toString());
+    //play some dank tunes when option is selected
+
 	@Override
 	public void setViewParent(ViewLoader viewPage) { //taken from ViewInterface since it's a pure virtual class
 		loader = viewPage; //just pass in whatever is given
@@ -29,10 +32,12 @@ public class MainMenuViewController implements Initializable, ViewInterface { //
 	@FXML
 	private void switchToInGameView(ActionEvent event) { //defined in FXML as the 'single player' button (For now)
 		loader.setView(CurrentViewWithMain.InGameViewID);
+		modeSelect.play();
 	}
 	
 	@FXML
 	private void switchToOptionsView(ActionEvent event) { //defined in FXML as the 'options' button (For now)
 		loader.setView(CurrentViewWithMain.OptionsViewID);
+		modeSelect.play();
 	}
 }
